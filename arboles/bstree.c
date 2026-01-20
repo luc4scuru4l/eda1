fad6c7cf3650071616d8c61ab2808b40a8ff609b
luc4scuru4l <lucascurual97@gmail.com>
2026-01-20 14:32:18 -0300

    Arboles: Se agrega bstree_recorrer y bstree_insertar

--- arboles/bstree.c
+++ arboles/bstree.c
@@ -1,5 +1,7 @@
 #include "../arboles/gbtree.h"
 #include "bstree.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 BSTree bstree_crear(){
   return gbtree_crear();
@@ -21,5 +23,41 @@ int bstree_buscar(BSTree arbol, void* dato, FuncionComparadora comp){
 }
 
 BSTree bstree_insertar(BSTree arbol, void * dato, FuncionCopiadora copy, FuncionComparadora comp){
+  if(gbtree_empty(arbol)){
+    BSTree nuevo = malloc(sizeof(struct _GBTree));
+    if(NULL == nuevo){
+      puts("Error de asignación de memoria");
+      return NULL;
+    }
+    nuevo->dato = copy(dato);
+    nuevo->left = gbtree_crear();
+    nuevo->right = gbtree_crear();
+    return nuevo;
+  }
 
+  int comparacion = comp(dato, arbol->dato);
+
+  if(comparacion < 0)
+    arbol->left = bstree_insertar(arbol->left, dato, copy, comp);
+  if(comparacion > 0)
+    arbol->right = bstree_insertar(arbol->right, dato, copy, comp);
+  return arbol;
+}
+
+void bstree_recorrer(GBTree arbol, BTreeOrdenDeRecorrido recorrido, FuncionVisitanteExtra visit, void *extra){
+  if(gbtree_empty(arbol))
+    return;
+
+  if(BTREE_RECORRIDO_PRE == recorrido)
+    visit(arbol->dato, extra);
+
+  bstree_recorrer(arbol->left, recorrido, visit, extra);
+
+  if(BTREE_RECORRIDO_IN == recorrido)
+    visit(arbol->dato, extra);
+  
+  bstree_recorrer(arbol->right, recorrido, visit, extra);
+
+  if(BTREE_RECORRIDO_POST == recorrido)
+    visit(arbol->dato, extra);
 }
---
+++ arboles/test2.c
@@ -0,0 +1,98 @@
+#include "bstree.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Casos de prueba para arboles de busqueda binaria generales
+ */
+
+#define N_PALABRAS 16
+#define N_NUMEROS 11
+
+static void *copiar_cadena(void *dato) {
+  char *str = malloc(sizeof(char) * (strlen(dato) + 1));
+  assert(str != NULL);
+  strcpy(str, dato);
+  return str;
+}
+
+static void* copiar_entero(void *dato) {
+  int *n = malloc(sizeof(int));
+  assert(n != NULL);
+  *n = *((int*) dato);
+  return n;
+}
+
+static int comparar_cadena(void *dato1, void *dato2) {
+  return strcmp(dato1, dato2);
+}
+
+static int comparar_entero(void *dato1, void *dato2) {
+  int a = *((int*) dato1);
+  int b = *((int*) dato2);
+  if (a == b)
+    return 0;
+  if(a > b)
+    return 1;
+  return -1;
+}
+
+static void destruir_cadena(void *dato) { free(dato); }
+
+static void destruir_entero(void *dato) { free(dato); }
+
+static void imprimir_cadena(void *dato, __attribute__((unused)) void *extra) {
+  /* __attribute__((unused)) le dice al compilador que esta variable puede no
+   * ser usada, de esta forma las banderas no tiran warnings.*/
+  printf("\"%s\" ", (char *)dato);
+}
+
+static void imprimir_entero(void *dato, __attribute__((unused)) void *extra) {
+  /* __attribute__((unused)) le dice al compilador que esta variable puede no
+   * ser usada, de esta forma las banderas no tiran warnings.*/
+  printf("%d ", *((int*) dato));
+}
+int main() {
+
+  char *palabras[N_PALABRAS] = {"gato",      "perro",    "casa",     "persona",
+                                "parque",    "arbol",    "edificio", "calle",
+                                "argentina", "santa fe", "rosario",  "unr",
+                                "edya",      "farmacia", "tiempo",   "celular"};
+  
+  int numeritos[N_NUMEROS] = {15, 9, 20, 6, 14, 17, 64, 13, 26, 72, 1};
+
+  // Creamos un arbol vacio y le insertamos las palabras
+  BSTree arbol = bstree_crear();
+  for (int i = 0; i < N_PALABRAS; i++)
+    arbol = bstree_insertar(arbol, palabras[i], copiar_cadena, comparar_cadena);
+
+  // Imprimir el arbol inorden (alfabetico)
+  printf("Recorrido inorden: ");
+  bstree_recorrer(arbol, BTREE_RECORRIDO_IN, imprimir_cadena, NULL);
+  puts("");
+
+  // Buscar elementos
+  assert(bstree_buscar(arbol, "farmacia", comparar_cadena) == 1);
+  assert(bstree_buscar(arbol, "santa fe", comparar_cadena) == 1);
+  assert(bstree_buscar(arbol, "persona", comparar_cadena) == 1);
+  assert(bstree_buscar(arbol, "unr", comparar_cadena) == 1);
+  assert(bstree_buscar(arbol, "argentina", comparar_cadena) == 1);
+  assert(bstree_buscar(arbol, "telefono", comparar_cadena) == 0);
+  assert(bstree_buscar(arbol, "mail", comparar_cadena) == 0);
+  assert(bstree_buscar(arbol, "parques", comparar_cadena) == 0);
+  assert(bstree_buscar(arbol, "EDyA1", comparar_cadena) == 0);
+
+  // Destruir arbol
+  bstree_destruir(arbol, destruir_cadena);
+
+  BSTree arbol_n = bstree_crear();
+  for (int i = 0; i < N_NUMEROS; i++)
+    arbol_n = bstree_insertar(arbol_n, &numeritos[i], copiar_entero, comparar_entero);
+
+  printf("Recorrido inorden: ");
+  bstree_recorrer(arbol_n, BTREE_RECORRIDO_IN, imprimir_entero, NULL);
+  bstree_destruir(arbol_n, destruir_entero);
+  return 0;
+}
